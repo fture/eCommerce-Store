9492caeed30f64ac7cff47c7aae12dbb87eca744
fture <hyonjun311@gmail.com>
2023-03-26 04:37:13 +0900

    alembic 마이그레이션

---
+++ alembic/versions/58da6be27401_moels_py.py
@@ -0,0 +1,24 @@
+"""moels.py
+
+Revision ID: 58da6be27401
+Revises: 9dd8d81ac12f
+Create Date: 2023-03-26 04:31:01.851050
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '58da6be27401'
+down_revision = '9dd8d81ac12f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
