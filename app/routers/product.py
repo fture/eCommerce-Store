b6b29c550dc5455efca0795f2cfe1b5c593377fd
fture <hyonjun311@gmail.com>
2023-03-29 00:14:42 +0900

    상품 app

--- app/routers/product.py
+++ app/routers/product.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from sqlalchemy import update
 from uuid import uuid4
 from typing import Optional
-
 from .. import models, schemas
 from ..database import get_db
 
@@ -17,13 +17,13 @@ async def get_all_products(
     skip: int = 0,
     serch: Optional[str] = "",
 ) -> list[schemas.ProductOut]:
-    stmt = (
+    smtp = (
         select(models.Product)
         .filter(models.Product.product_name.contains(serch))
         .limit(limit)
         .offset(skip)
     )
-    result = await db.execute(stmt)
+    result = await db.execute(smtp)
     all_products = result.scalars().all()
     return all_products
 
@@ -32,11 +32,11 @@ async def get_all_products(
 async def create_product(
     product_data: schemas.ProductBase, db: AsyncSession = Depends(get_db)
 ) -> schemas.ProductOut:
-    result = await db.execute(
-        select(models.Product).filter(
-            models.Product.product_name == product_data.product_name
-        )
+    smtp = select(models.Product).filter(
+        models.Product.product_name == product_data.product_name
     )
+    result = await db.execute(smtp)
+
     if result.scalar():
         raise HTTPException(
             status_code=404,
@@ -47,3 +47,44 @@ async def create_product(
     db.add(product_post)
     await db.commit()
     return product_post
+
+
+@router.get("/{product_id}", status_code=status.HTTP_200_OK)
+async def update_product(
+    product_id: str, db: AsyncSession = Depends(get_db)
+) -> schemas.ProductOut:
+    target_result = await db.get(models.Product, product_id)
+    if not target_result:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    return target_result
+
+
+@router.put("/{product_id}", status_code=status.HTTP_200_OK)
+async def update_product(
+    product_id: str,
+    product_data: schemas.ProductUpdate,
+    db: AsyncSession = Depends(get_db),
+) -> schemas.ProductOut:
+    product = await db.get(models.Product, product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    for key, value in product_data.dict(exclude_unset=True).items():
+        setattr(product, key, value)
+
+    await db.commit()
+    await db.refresh(product)
+    return product
+
+
+@router.delete("/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete(product_id: str, db: AsyncSession = Depends(get_db)):
+    target_product = await db.get(models.Product, product_id)
+    if not target_product:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Product_id {product_id} not found",
+        )
+
+    await db.commit()
+    return {"msg": "Delete product successed"}
