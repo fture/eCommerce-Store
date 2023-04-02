4f2ea1cc54c09f106ce10bbb1e2bd0f9c83971cc
fture <hyonjun311@gmail.com>
2023-04-02 22:30:47 +0900

    product app

--- app/routers/customer.py
+++ app/routers/customer.py
@@ -36,13 +36,46 @@ async def create_user(
 # PRIVATE METHODS
 
 
-@router.get("/{email}", status_code=status.HTTP_200_OK)
-async def get_user(email: str, db: AsyncSession = Depends(get_db)):
-    stmp = select(models.Customer).filter(models.Customer.email == email)
-    user = await db.execute(stmp)
-    return user.scalar()
+@router.get("/{customer_id}", status_code=status.HTTP_200_OK)
+async def get_user(customer_id: str, db: AsyncSession = Depends(get_db)):
+    try:
+        stmp = select(models.Customer).filter(models.Customer.email == customer_id)
+        user = await db.execute(stmp)
+        return user.scalar()
+    except:
+        raise HTTPException(status_code=404, detail={"msg": "Customer does not exist"})
 
 
 @router.put("/{email}", status_code=status.HTTP_200_OK)
-async def update_user(email: str, password: str, db: AsyncSession = Depends(get_db)):
-    pass
+async def update_user(
+    email: str,
+    customer_data: schemas.CustomerUpdate,
+    db: AsyncSession = Depends(get_db),
+):
+    stmp = select(models.Customer).filter(models.Customer.email == email)
+    user = await db.execute(stmp)
+    user_data = user.scalar()
+    for key, value in customer_data.dict(exclude_unset=True).items():
+        setattr(user_data, key, value)
+    db.add(user_data)
+    await db.commit()
+    return user_data
+
+
+@router.delete("/{customer_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(customer_id: str, db: AsyncSession = Depends(get_db)):
+    try:
+        target_customer = await db.get(models.Customer, customer_id)
+        if not target_customer:
+            raise HTTPException(
+                status_code=404,
+                detail=f"customer not found",
+            )
+    except:
+        raise HTTPException(
+            status_code=404,
+            detail=f"customer {customer_id} is not found",
+        )
+    await db.delete(target_customer)
+    await db.commit()
+    return {"msg": "Delete customer successed"}
